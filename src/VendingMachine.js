0f643e1052288de8fa5fe82fee8abd7ee49d399a
shinya_saita <shinya_saita@lockon.co.jp>
2017-06-29 21:01:09 +0900

    Step0 complete!

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,4 +1,29 @@
-function VendingMachine() {}
-VendingMachine.prototype.insert = function() {return 10}
-module.exports = VendingMachine;
+function VendingMachine() {
+	this.coinBuffer = [];
+}
+VendingMachine.prototype.insert = function(value) {
+	this.coinBuffer.push(value);
+	return value
+}
+VendingMachine.prototype.getTotal = function() {
+	var total = 0;
+    for (var i in this.coinBuffer) {
+		total += this.coinBuffer[i];
+	}
+	return total;
+}
+VendingMachine.prototype.refund = function() {
+	var result = this.coinBuffer;
+	this.coinBuffer = [];
+	return result;
+}
+module.exports.VendingMachine = VendingMachine;
 
+var Coin = {
+	'ten':10,
+	'fifty':50,
+	'oneHundred':100,
+	'fiveHundred':500,	
+	'oneThousand':1000	
+}; 		
+module.exports.Coin = Coin
--- test/hoge.js
+++ test/hoge.js
@@ -1,14 +1,44 @@
 const assert = require("assert");
-const VendingMachine = require("../src/VendingMachine.js");
+const VendingMachine = require("../src/VendingMachine.js").VendingMachine;
+const Coin = require("../src/VendingMachine.js").Coin;
 describe("hage", function() {
 	it("VendingMachineをnewできる", function() {
 		assert(VendingMachine)
 	});
 	it("10yen玉を１枚投入できる", function() {
 		var vm = new VendingMachine();
-		var Coin = {
-			'ten':10	
-		} ; 		
 		assert.equal(10,vm.insert(Coin.ten))
+	});
+
+	it("コインを全種類投入できる", function() {
+		var vm = new VendingMachine();
+		assert.equal(10,vm.insert(Coin.ten))
+		assert.equal(50,vm.insert(Coin.fifty))
+		assert.equal(100,vm.insert(Coin.oneHundred))
+		assert.equal(500,vm.insert(Coin.fiveHundred))
+		assert.equal(1000,vm.insert(Coin.oneThousand))
+	});
+	it("合計金額を取得できる", function() {
+		var vm = new VendingMachine();
+		vm.insert(Coin.ten)
+		vm.insert(Coin.fifty)
+		vm.insert(Coin.oneHundred)
+		vm.insert(Coin.fiveHundred)
+		vm.insert(Coin.oneThousand)
+		assert.equal(1660, vm.getTotal())
+	})
+	it("払い戻しができる", function() {
+		var vm = new VendingMachine();
+		vm.insert(Coin.ten)
+		vm.insert(Coin.fifty)
+
+		assert.deepEqual([Coin.ten, Coin.fifty], vm.refund());
+	});
+	it("払い戻したら総額が0円になる", function() {
+		var vm = new VendingMachine();
+		vm.insert(Coin.ten)
+		vm.insert(Coin.fifty)
+		vm.refund();
+		assert.equal(0, vm.getTotal());
 	});
 });
